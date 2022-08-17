46e8a6cbb336c153403c554cf1429021d4549b93
franco77777 <francoaresis@gmail.com>
2022-08-17 01:13:00 -0300

    loader

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,32 @@ import Details from "./components/Details"
 import Home from "./components/Home"
 import Activity from "./components/Activity"
 import Intro from "./components/Intro"
+import { useDispatch, useSelector } from "react-redux"
+import Loader from "./components/Loader"
+import { getCountries } from "./actions"
+import { useEffect } from "react"
 
 function App() {
+  const countries = useSelector(state => state.countries)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getCountries())
+  }, [])
   return (
     <Routes>
       <Route path="/" element={<Intro />} />
-      <Route path="/country" element={<Home />} />
-      <Route path="/country/:id" element={<Details />} />
-      <Route path="/activity" element={<Activity />} />
+      <Route
+        path="/country"
+        element={countries.length ? <Home /> : <Loader />}
+      />
+      <Route
+        path="/country/:id"
+        element={countries.length ? <Details /> : <Loader />}
+      />
+      <Route
+        path="/activity"
+        element={countries.length ? <Activity /> : <Loader />}
+      />
     </Routes>
   )
 }
---
+++ client/src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./loader.css"
+
+const Loader = () => {
+  return (
+    <article className="bodyloader">
+      <section className="loadericon">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </section>
+    </article>
+  )
+}
+
+export default Loader
